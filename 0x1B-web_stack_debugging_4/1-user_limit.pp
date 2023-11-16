26473de02080621d89f9ab4ad00ae8d5cde19167
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-11-16 16:00:49 +0200

    as

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+# Enable the user holberton to login 
+
 # Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
