2769fbe606965283e52fdbe4e647272b3ff8979f
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-11-16 15:50:47 +0200

    w

---
+++ 0x1B-web_stack_debugging_4/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//asdasdasd
+typedef struct node_s {
+	int data;
+	struct node_s *next;
+	struct node_s *prev;
+} node_t;
+
+int main(int argc, char **argv)
+{
+	printf("Hello, World!\n");
+	return (0);
+}
